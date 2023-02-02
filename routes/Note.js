2aff5ba1505cb637a346e51b2ca50e5f47fd4cc4
Sam Sweeney <samsweeneyy@gmail.com>
2023-02-02 21:15:06 +1100

    Updated note route to include user

--- routes/Note.js
+++ routes/Note.js
@@ -1,24 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const {addNote, getAllPublicNotes, getAllUserNotes, deletePublicNote, deleteUserNote} = require('../lib/db/db.notes');
+const { getUser } = require("../middleware/getUser");
 
-router.post('/', async (req, res) => {
+
+router.post('/', getUser, async (req, res) => {
     const {title, text} = req.body;
-    await addNote({title, text});
+    const user = req.user;
+    await addNote({title, text, userId: user ? user.id : null});
     res.sendStatus(200);
 })
 
 router.get('/', getUser, async (req, res) => {
-    let notes = await getAllPublicNotes();
+    const user = req.user;
+    let notes = [];
+    if (user) {
+        notes = await getAllUserNotes(user.id)
+    } else {
+        notes = await getAllPublicNotes();
+    }
     res.json(notes);
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', getUser, (req, res) => {
     const {id} = req.params
     if (!id) return res.sendStatus(400);
-    await deletePublicNote(id);
-    res.sendStatus(200);
+    if (req.user) {
+        deleteUserNote(id, req.user.id)
+        res.sendStatus(200);
+    } else {
+        deletePublicNote(id)
+        res.sendStatus(200);
+    }
 })
-
 
 module.exports = router;
