e9a305fe0c5c117cabc57d72917943d44f6bb9b0
Sam Sweeney <samsweeneyy@gmail.com>
2023-02-02 21:15:43 +1100

    Added user route and cookieparser to server

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
-const notesRouter = require('./routes/Note')
+const userRouter = require('./routes/User');
+const notesRouter = require('./routes/Notes');
+require("dotenv").config()
+
+
 const app = express();
 
 
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(express.urlencoded({extended: true}));
 
+// Routes
+
+app.use("/api/user", userRouter);
+app.use("/api/notes", notesRouter);
 
 
-app.use('/api/notes', mn)
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.listen(process.env.port || 3000);
+
+
+app.listen(process.env.port || 3000)
+
+
+
